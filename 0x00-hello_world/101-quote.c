5f116b508ad0c488a730115260017af5e45bc112
umlamulankunzi <studentjele@gmail.com>
2023-02-16 10:54:43 -0800

    corrections to advanced question solutions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-	fprint(stderr, "and that piece of art is useful\" - Dora Korpar, "
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, "
 	"2015-10-19");
 	return (1);
 }
