a710b06fde9dca7ca5bb9cb0234fe9bb46688367
umlamulankunzi <studentjele@gmail.com>
2023-02-16 10:52:26 -0800

    advanced scripts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 if successful else 1
+*/
+int main(void)
+{
+	fprint(stderr, "and that piece of art is useful\" - Dora Korpar, "
+	"2015-10-19");
+	return (1);
+}
+
