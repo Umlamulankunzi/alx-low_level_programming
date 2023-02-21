ddb20b3e2b43a07e020704fea899b295f5f4667c
Prince D Jele <diehard@localhost.localdomain>
2023-02-21 16:35:49 +0200

    solution to task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
 * main - Entry point of program
 *
@@ -6,6 +8,14 @@
 
 int main()
 {
-    puts("_putchar");
-    return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
