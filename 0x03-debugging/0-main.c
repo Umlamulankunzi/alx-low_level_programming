46fc2bbe69258e696c7624c3643c1eec6ae1f7a2
Prince D Jele <diehard@localhost.localdomain>
2023-02-22 12:50:33 +0200

    created directory and solution code

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - function to test function positive_or_negative(int) corrent output
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
