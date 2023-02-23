85f63638793ebf35bf96a05da794a20cf881e987
umlamulankunzi <studentjele@gmail.com>
2023-02-23 06:31:49 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _isdigit - multiply 2 numbers
+* @c: int
+* @b: int
+* Description: @c checked if is digit
+* Return: product of parameters @a & @b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
