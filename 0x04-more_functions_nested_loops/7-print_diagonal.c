ffd9c8476af679385c1ad12cf56a64452e069ea1
umlamulankunzi <studentjele@gmail.com>
2023-02-23 08:12:51 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_diagonal - print \ @n times
+* @n: int
+* Description: print \ char @n times if #n > 0
+*/
+void print_diagonal(int n)
+{
+	int count;
+	int spaces;
+
+	count = 0;
+
+	while (count < n)
+	{
+		for (spaces = 0; spaces < count; spaces++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+		count++;
+	}
+}
+
