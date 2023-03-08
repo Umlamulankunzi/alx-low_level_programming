3eda164e050b7f2843f96a95804f39c0f8354343
Prince D Jele <diehard@localhost.localdomain>
2023-03-08 19:37:44 +0200

    created directory & task 0

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	_puts_recursion("The quick brown fox");
+	_puts_recursion("jumped over the lazy dog");
+	return (0);
+}
