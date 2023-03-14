24bbd9594f7b65594db6192c73dbd78c3a73b9c3
umlamulankunzi <umlamulankunzi@gmail.com>
2023-03-14 17:54:01 +0200

    completed mandatory tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,36 +10,31 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str_ptr;
-	int s1_size, s2_size, i;
+	int i, j, k, max;
 
-	if (s1 != NULL)
-		for (s1_size = 0; s1[s1_size] != '\0'; s1_size++);
-	else
+	if (s1 == NULL)
 		s1 = "";
 
-	if (s2 != NULL)
-		for (s2_size = 0; s2[s2_size] != '\0'; s2_size++);
-	else
+	if (s2 == NULL)
 		s2 = "";
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		return (NULL);
-	}
-	str_ptr = malloc(((s1_size + s2_size) * sizeof(char)) + 1);
+	for (i = 0; s1[i] != '\0'; i++);
+	for (j = 0; s2[j] != '\0'; j++);
+
+	str_ptr = malloc(sizeof(char) * (i + j + 1));
 
 	if (str_ptr == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < s1_size; i++)
+	for (k = 0; k < i; k++)
+		str_ptr[k] = s1[k];
+
+	max = j;
+	for (j = 0; j <= max; j++)
 	{
-		str_ptr[i] = s1[i];
+		str_ptr[k] = s2[j];
+		k++;
 	}
-	for (; i < (s1_size + s2_size); i++)
-	{
-		str_ptr[i] = s2[i - s1_size];
-	}
-	str_ptr[i] = '\0';
 	return (str_ptr);
 }
