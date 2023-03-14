5a9cba62de5c835f410c4f8488122e33be2c0352
umlamulankunzi <umlamulankunzi@gmail.com>
2023-03-14 17:55:03 +0200

    completed mandatory tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,8 +18,10 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++);
-	for (j = 0; s2[j] != '\0'; j++);
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
 	str_ptr = malloc(sizeof(char) * (i + j + 1));
 
