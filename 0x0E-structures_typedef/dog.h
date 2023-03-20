c8f96e133273395df61304c982b2a43f4b0a8962
umlamulankunzi <studentjele@gmail.com>
2023-03-20 08:07:41 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Dog struct
+* @name: string
+* @age: float
+* @owner: string
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+typedef struct dog dog;
+
+#endif
+
