90e4721205bbac92d47c449a410a1a4535b2c45c
Prince D Jele <diehard@localhost.localdomain>
2023-03-22 23:17:13 +0200

    update task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,6 +19,12 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	op = get_op_func(argv[2]);
