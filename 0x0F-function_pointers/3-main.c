bc76f7d2c283c7afc84f86f0e397034138c52d31
Prince D Jele <diehard@localhost.localdomain>
2023-03-22 22:57:27 +0200

    task 3 completed

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+* main - Starting point of program
+* @argc: int size of argc array
+* @argv: array of char * of arguments to program
+* Return: 0 if successful
+*/
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Too many args\n");
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = get_op_func(argv[2]);
+
+	if (op)
+		printf("%d\n", op(num1, num2));
+	else
+	{
+		printf("%s", argv[2]);
+		printf("Error\n");
+		exit (99);
+	}
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+
+/**
+* op_add - add two integers
+* @a: int
+* @b: int
+* Return: int sum of @a and @b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+* op_sub - subtract two integers
+* @a: int
+* @b: int
+* Return: int difference of @a and @b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+* op_mul - multiply two integers
+* @a: int
+* @b: int
+* Return: int product of @a and @b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+
+/**
+* op_div - divide two integers
+* @a: int
+* @b: int
+* Return: int division result of @a and @b
+*/
+int op_div(int a, int b)
+{
+	if (b)
+		return (a / b);
+	exit(100);
+}
+
+
+/**
+* op_add - modulo divide two integers
+* @a: int
+* @b: int
+* Return: int remainder of division of @a and @b
+*/
+int op_mod(int a, int b)
+{
+	if (b)
+		return (a % b);
+	exit(100);
+}
