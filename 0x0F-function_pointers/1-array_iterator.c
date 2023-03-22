fb35aac10621dda9601c6576224e972853e1972b
umlamulankunzi <studentjele@gmail.com>
2023-03-22 11:53:14 -0700

    update task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+
+
+/**
+* array_iteratir - apply @action to elements of @array
+* @array: int array
+* @size: size of @array
+* @action: f\nction pointer to callback
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	i = 0;
+
+	
+}
