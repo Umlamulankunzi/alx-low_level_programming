dcd3f655bd03d400a2b27c475d867445772dbc88
Prince D Jele <diehard@localhost.localdomain>
2023-04-04 04:55:29 +0200

    first advanced task

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+
+/**
+* reverse_listint - reverse a linked list
+* @head: pointer to address of linked list head node
+* Return: pointer to head node of reversed list.
+*/
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev_node, *fwd_node;
+
+	if (*head == NULL || head == NULL)
+		return (NULL);
+
+	prev_node = NULL		/** starts at NULL signifying end node */
+	while((*head)->next)
+	{
+		fwd_node = (*head)->next;
+		(*head)->next = prev_node;
+		prev_node = *head;
+		*head = fwd_node;
+	}
+	(*head)->next = prev_node;
+	return (*head);
+}
