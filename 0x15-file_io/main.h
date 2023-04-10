9521a9b5ffb3a681cd5960e8b21d4811274a7465
umlamulankunzi <umlamulankunzi@gmail.com>
2023-04-10 22:29:47 +0200

    completed task 0

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
