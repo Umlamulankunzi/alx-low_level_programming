a1d5ed29522efa2feb19c2420ea495fa24fb943e
Prince D Jele <31880807+Umlamulankunzi@users.noreply.github.com>
2023-05-08 20:49:05 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-
+#include "main.h"
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of the file to read
