150f3adaaac5c27b36b1576d550b791321bf37e8
Prince D Jele <31880807+Umlamulankunzi@users.noreply.github.com>
2023-05-08 21:04:21 +0200

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,7 @@
 
 /* Function prototypes */
 
+int _putchar(char c);
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 
